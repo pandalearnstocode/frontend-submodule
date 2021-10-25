8d79e70ce9ad47c623387828abe4d6a5dab252dc
Aritra Biswas <pandalearnstocode@gmail.com>
2021-10-26 01:27:11 +0530

    adding port.

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_URL_LOCAL = os.getenv('API_URL_LOCAL')
 API_URL_DOCKER = os.getenv('API_URL_DOCKER')
 DEPLOYMENT = os.getenv('deployment')
-if deployment == "docker":
+if DEPLOYMENT == "docker":
     API_URL = API_URL_DOCKER
 else:
     API_URL = API_URL_LOCAL
