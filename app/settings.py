42ee55088b507c28eed9c58ca81aae7b881ff070
Aritra Biswas <pandalearnstocode@gmail.com>
2021-10-26 01:14:04 +0530

    adding settings for local and docker url.

--- app/settings.py
+++ app/settings.py
@@ -3,4 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_URL = os.getenv('API_URL')
+API_URL_LOCAL = os.getenv('API_URL_LOCAL')
+API_URL_DOCKER = os.getenv('API_URL_DOCKER')
+DEPLOYMENT = os.getenv('deployment')
+if deployment == "docker":
+    API_URL = API_URL_DOCKER
+else:
+    API_URL = API_URL_LOCAL
